c7f43427981f8f71c46f188000b79f15a0a63a82
Marina Ballester <marinabir94@gmail.com>
2021-02-28 18:58:24 +0100

    added global styles

---
+++ src/components/App/index.js
@@ -0,0 +1,28 @@
+//https://www.youtube.com/watch?v=oGB_VPrld0U&list=PLTTC1K14KAxHj6AftnRUD28SQaoVauvl3&index=1
+//https://gitconnected.com/courses/learn-react-redux-tutorial-build-a-hacker-news-clone
+//https://github.com/gitconnected/hacker-news-reader/tree/master/src
+
+import logo from "./logo.svg";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
