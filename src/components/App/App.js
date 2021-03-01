5215339660f6e9124762db4f205c33d6d7a7de70
Marina Ballester <marinabir94@gmail.com>
2021-03-01 20:55:20 +0100

    add List components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,27 +1,24 @@
 //https://www.youtube.com/watch?v=oGB_VPrld0U&list=PLTTC1K14KAxHj6AftnRUD28SQaoVauvl3&index=1
 //https://gitconnected.com/courses/learn-react-redux-tutorial-build-a-hacker-news-clone
 //https://github.com/gitconnected/hacker-news-reader/tree/master/src
-
+import React, { Component } from 'react';
 import logo from "./logo.svg";
+
+import { ThemeProvider } from 'styled-components';
+import { colorsDark } from '../../styles/palette' 
+import { Wrapper, Title } from './styles';
+import List  from 'components/List';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={colorsDark}>
+      <div>
+        <Wrapper>
+          <Title>Reddit Minimal Clone</Title>
+          <List />
+        </Wrapper>
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/ListItem/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { Item, Title, Host, ExternalLink, Description, CommentsLink } from './styles';
+
+const LINK_REL = 'nooper noreferrer nofollow'
+
+const ListItem = () => {
+    return (
+        <Item>
+            <ExternalLink>
+                <Title>
+                    The Developer Community <Host>(gitconnected.com)</Host>
+                </Title>
+            </ExternalLink>
+            <Description>
+                <CommentsLink href='#' rel={LINK_REL} target='_blank'>
+                marinaballester
+                </CommentsLink>
+                1 hour ago {' | '}
+                <CommentsLink href='#' rel={LINK_REL} target='_blank'>
+                42 comments
+                </CommentsLink>
+            </Description>
+        </Item>
+    )
+}
+
+export default ListItem;
